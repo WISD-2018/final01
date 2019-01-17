db559f2317c44647697c37f27244af6fb7e795ff
3A532066 <bcdling05@gmail.com>
2019-01-17 23:57:07 +0800

    設計登入畫面並修改首面

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
             .links > a {
                 color: #000000;
                 padding: 0 25px;
-                font-size: 13px;
+                font-size: 20px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
